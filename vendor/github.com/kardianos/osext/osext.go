84f01df88d245e3a2b4483457144a87db028cf89
Jimmi Dyson <jimmidyson@gmail.com>
2016-07-21 20:06:02 +0100

    Upgrade to Kubernetes 1.3.2

--- vendor/github.com/kardianos/osext/osext.go
+++ vendor/github.com/kardianos/osext/osext.go
@@ -7,27 +7,21 @@ package osext
 
 import "path/filepath"
 
-var cx, ce = executableClean()
-
-func executableClean() (string, error) {
-	p, err := executable()
-	return filepath.Clean(p), err
-}
-
 // Executable returns an absolute path that can be used to
 // re-invoke the current program.
 // It may not be valid after the current program exits.
 func Executable() (string, error) {
-	return cx, ce
+	p, err := executable()
+	return filepath.Clean(p), err
 }
 
 // Returns same path as Executable, returns just the folder
-// path. Excludes the executable name and any trailing slash.
+// path. Excludes the executable name.
 func ExecutableFolder() (string, error) {
 	p, err := Executable()
 	if err != nil {
 		return "", err
 	}
-
-	return filepath.Dir(p), nil
+	folder, _ := filepath.Split(p)
+	return folder, nil
 }

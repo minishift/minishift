b3b47eef61b337c919c156a2656dc2a1692e5bb2
Dan Lorenc <lorenc.d@gmail.com>
2016-08-06 12:29:56 -0700

    Return a MultiError from retry.

--- pkg/util/utils.go
+++ pkg/util/utils.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -67,12 +68,36 @@ func Retry(attempts int, callback func() error) (err error) {
 }
 
 func RetryAfter(attempts int, callback func() error, d time.Duration) (err error) {
+	m := MultiError{}
 	for i := 0; i < attempts; i++ {
 		err = callback()
 		if err == nil {
 			return nil
 		}
+		m.Collect(err)
 		time.Sleep(d)
 	}
-	return err
+	return m.ToError()
+}
+
+type MultiError struct {
+	Errors []error
+}
+
+func (m *MultiError) Collect(err error) {
+	if err != nil {
+		m.Errors = append(m.Errors, err)
+	}
+}
+
+func (m MultiError) ToError() error {
+	if len(m.Errors) == 0 {
+		return nil
+	}
+
+	errStrings := []string{}
+	for _, err := range m.Errors {
+		errStrings = append(errStrings, err.Error())
+	}
+	return fmt.Errorf(strings.Join(errStrings, "\n"))
 }

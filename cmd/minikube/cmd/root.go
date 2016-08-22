kubernetes/minikube

Add env variables for viper controlled vars
Minikube will now read from env variables with the MINIKUBE_ prefix.
These variables will be read on every viper.Get() and will overwrite default
variables.  When we add binding to pflags, flags will overwrite env
variables.  At this time, only notification settings are controlled by
viper.

cmd/minikube/cmd/root.go
@@ -81,6 +81,11 @@ func init() {
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	configPath := constants.MakeMiniPath("config")
+
+	// Bind all viper values to env variables
+	viper.SetEnvPrefix(constants.MinikubeEnvPrefix)
+	viper.AutomaticEnv()
+
 	viper.SetConfigName("config")
 	viper.AddConfigPath(configPath)
 	err := viper.ReadInConfig()

minishift/minishift

cmd/minikube/cmd/root.go
@@ -82,6 +82,11 @@ func init() {
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	configPath := constants.MakeMiniPath("config")
+
+	// Bind all viper values to env variables
+	viper.SetEnvPrefix(constants.MiniShiftEnvPrefix)
+	viper.AutomaticEnv()
+
 	viper.SetConfigName("config")
 	viper.AddConfigPath(configPath)
 	err := viper.ReadInConfig()
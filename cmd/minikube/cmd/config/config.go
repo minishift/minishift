kubernetes/minikube

Add minikube config get/set/unset commands
This introduces the minikube config get/set/unset command. At a basic level, it allows a more user friendly interface for setting minikube config values, but it also allows us to run validations and callbacks before and after the values have been set.

cmd/minikube/cmd/config/config.go
@@ -1,0 +1,152 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"os"
+
+	"github.com/spf13/cobra"
+	"k8s.io/minikube/pkg/minikube/constants"
+)
+
+type configFile interface {
+	io.ReadWriter
+}
+
+type setFn func(string, string) error
+type MinikubeConfig map[string]interface{}
+
+type Setting struct {
+	name        string
+	set         func(MinikubeConfig, string, string) error
+	validations []setFn
+	callbacks   []setFn
+}
+
+// These are all the settings that are configurable
+// and their validation and callback fn run on Set
+var settings []Setting = []Setting{
+	{
+		name:        "vm-driver",
+		set:         SetString,
+		validations: []setFn{IsValidDriver},
+		callbacks:   []setFn{RequiresRestartMsg},
+	},
+	{
+		name:        "v",
+		set:         SetInt,
+		validations: []setFn{IsPositive},
+	},
+	{
+		name:        "cpus",
+		set:         SetInt,
+		validations: []setFn{IsPositive},
+		callbacks:   []setFn{RequiresRestartMsg},
+	},
+	{
+		name:        "disk-size",
+		set:         SetString,
+		validations: []setFn{IsValidDiskSize},
+		callbacks:   []setFn{RequiresRestartMsg},
+	},
+	{
+		name:        "host-only-cidr",
+		set:         SetString,
+		validations: []setFn{IsValidCIDR},
+	},
+	{
+		name:        "memory",
+		set:         SetInt,
+		validations: []setFn{IsPositive},
+		callbacks:   []setFn{RequiresRestartMsg},
+	},
+	{
+		name: "show-libmachine-logs",
+		set:  SetBool,
+	},
+	{
+		name:        "log_dir",
+		set:         SetString,
+		validations: []setFn{IsValidPath},
+	},
+	{
+		name: "kubernetes-version",
+		set:  SetString,
+	},
+}
+
+var ConfigCmd = &cobra.Command{
+	Use:   "config SUBCOMMAND [flags]",
+	Short: "Modify minikube config",
+	Long:  `config modifies minikube config files using subcommands like "minikube config set vm-driver kvm"`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+// Reads in the JSON minikube config
+func ReadConfig() (MinikubeConfig, error) {
+	f, err := os.Open(constants.ConfigFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return make(map[string]interface{}), nil
+		}
+		return nil, fmt.Errorf("Could not open file %s: %s", constants.ConfigFile, err)
+	}
+	var m MinikubeConfig
+	m, err = decode(f)
+	if err != nil {
+		return nil, fmt.Errorf("Could not decode config %s: %s", constants.ConfigFile, err)
+	}
+
+	return m, nil
+}
+
+// Writes a minikube config to the JSON file
+func WriteConfig(m MinikubeConfig) error {
+	f, err := os.Create(constants.ConfigFile)
+	if err != nil {
+		return fmt.Errorf("Could not open file %s: %s", constants.ConfigFile, err)
+	}
+	defer f.Close()
+	err = encode(f, m)
+	if err != nil {
+		return fmt.Errorf("Error encoding config %s: %s", constants.ConfigFile, err)
+	}
+	return nil
+}
+
+func decode(r io.Reader) (MinikubeConfig, error) {
+	var data MinikubeConfig
+	err := json.NewDecoder(r).Decode(&data)
+	return data, err
+}
+
+func encode(w io.Writer, m MinikubeConfig) error {
+	b, err := json.MarshalIndent(m, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(b)
+
+	return err
+}

cmd/minikube/cmd/config/set.go
@@ -1,0 +1,76 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var configSetCmd = &cobra.Command{
+	Use:   "set PROPERTY_NAME PROPERTY_VALUE",
+	Short: "Sets an individual value in a minikube config file",
+	Long: `Sets the PROPERTY_NAME config value to PROPERTY_VALUE
+	These values can be overwritten by flags or environment variables at runtime.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 2 {
+			fmt.Fprintln(os.Stderr, "usage: minikube config set PROPERTY_NAME PROPERTY_VALUE")
+			os.Exit(1)
+		}
+		err := set(args[0], args[1])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	ConfigCmd.AddCommand(configSetCmd)
+}
+
+func set(name string, value string) error {
+	s, err := findSetting(name)
+	if err != nil {
+		return err
+	}
+	// Validate the new value
+	err = run(name, value, s.validations)
+	if err != nil {
+		return err
+	}
+
+	// Set the value
+	config, err := ReadConfig()
+	if err != nil {
+		return err
+	}
+	err = s.set(config, name, value)
+	if err != nil {
+		return err
+	}
+
+	// Run any callbacks for this property
+	err = run(name, value, s.callbacks)
+	if err != nil {
+		return err
+	}
+
+	// Write the value
+	return WriteConfig(config)
+}

minishift/minishift

cmd/minikube/cmd/config/config.go
@@ -1,0 +1,153 @@
+/*
+Copyright (C) 2016 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/jimmidyson/minishift/pkg/minikube/constants"
+)
+
+type configFile interface {
+	io.ReadWriter
+}
+
+type setFn func(string, string) error
+type MinikubeConfig map[string]interface{}
+
+type Setting struct {
+	name        string
+	set         func(MinikubeConfig, string, string) error
+	validations []setFn
+	callbacks   []setFn
+}
+
+// These are all the settings that are configurable
+// and their validation and callback fn run on Set
+var settings []Setting = []Setting{
+	{
+		name:        "vm-driver",
+		set:         SetString,
+		validations: []setFn{IsValidDriver},
+		callbacks:   []setFn{RequiresRestartMsg},
+	},
+	{
+		name:        "v",
+		set:         SetInt,
+		validations: []setFn{IsPositive},
+	},
+	{
+		name:        "cpus",
+		set:         SetInt,
+		validations: []setFn{IsPositive},
+		callbacks:   []setFn{RequiresRestartMsg},
+	},
+	{
+		name:        "disk-size",
+		set:         SetString,
+		validations: []setFn{IsValidDiskSize},
+		callbacks:   []setFn{RequiresRestartMsg},
+	},
+	{
+		name:        "host-only-cidr",
+		set:         SetString,
+		validations: []setFn{IsValidCIDR},
+	},
+	{
+		name:        "memory",
+		set:         SetInt,
+		validations: []setFn{IsPositive},
+		callbacks:   []setFn{RequiresRestartMsg},
+	},
+	{
+		name: "show-libmachine-logs",
+		set:  SetBool,
+	},
+	{
+		name:        "log_dir",
+		set:         SetString,
+		validations: []setFn{IsValidPath},
+	},
+	{
+		name: "openshift-version",
+		set:  SetString,
+	},
+}
+
+var ConfigCmd = &cobra.Command{
+	Use:   "config SUBCOMMAND [flags]",
+	Short: "Modify minishift config",
+	Long:  `config modifies minishift config files using subcommands like "minishift config set vm-driver kvm"`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+// Reads in the JSON minikube config
+func ReadConfig() (MinikubeConfig, error) {
+	f, err := os.Open(constants.ConfigFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return make(map[string]interface{}), nil
+		}
+		return nil, fmt.Errorf("Could not open file %s: %s", constants.ConfigFile, err)
+	}
+	var m MinikubeConfig
+	m, err = decode(f)
+	if err != nil {
+		return nil, fmt.Errorf("Could not decode config %s: %s", constants.ConfigFile, err)
+	}
+
+	return m, nil
+}
+
+// Writes a minikube config to the JSON file
+func WriteConfig(m MinikubeConfig) error {
+	f, err := os.Create(constants.ConfigFile)
+	if err != nil {
+		return fmt.Errorf("Could not open file %s: %s", constants.ConfigFile, err)
+	}
+	defer f.Close()
+	err = encode(f, m)
+	if err != nil {
+		return fmt.Errorf("Error encoding config %s: %s", constants.ConfigFile, err)
+	}
+	return nil
+}
+
+func decode(r io.Reader) (MinikubeConfig, error) {
+	var data MinikubeConfig
+	err := json.NewDecoder(r).Decode(&data)
+	return data, err
+}
+
+func encode(w io.Writer, m MinikubeConfig) error {
+	b, err := json.MarshalIndent(m, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(b)
+
+	return err
+}

cmd/minikube/cmd/config/set.go
@@ -1,0 +1,77 @@
+/*
+Copyright (C) 2016 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var configSetCmd = &cobra.Command{
+	Use:   "set PROPERTY_NAME PROPERTY_VALUE",
+	Short: "Sets an individual value in a minishift config file",
+	Long: `Sets the PROPERTY_NAME config value to PROPERTY_VALUE
+	These values can be overwritten by flags or environment variables at runtime.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 2 {
+			fmt.Fprintln(os.Stderr, "usage: minishift config set PROPERTY_NAME PROPERTY_VALUE")
+			os.Exit(1)
+		}
+		err := set(args[0], args[1])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	ConfigCmd.AddCommand(configSetCmd)
+}
+
+func set(name string, value string) error {
+	s, err := findSetting(name)
+	if err != nil {
+		return err
+	}
+	// Validate the new value
+	err = run(name, value, s.validations)
+	if err != nil {
+		return err
+	}
+
+	// Set the value
+	config, err := ReadConfig()
+	if err != nil {
+		return err
+	}
+	err = s.set(config, name, value)
+	if err != nil {
+		return err
+	}
+
+	// Run any callbacks for this property
+	err = run(name, value, s.callbacks)
+	if err != nil {
+		return err
+	}
+
+	// Write the value
+	return WriteConfig(config)
+}
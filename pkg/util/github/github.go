7822fef338e27da9eaee62bf1ab3eb2419fe4a48
Jimmi Dyson <jimmidyson@gmail.com>
2016-10-14 12:59:03 +0100

    Validate checksums for downloaded cached files

--- pkg/util/github/github.go
+++ pkg/util/github/github.go
@@ -18,7 +18,10 @@ package github
 
 import (
 	"archive/tar"
+	"bufio"
 	"compress/gzip"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -85,10 +88,11 @@ func DownloadOpenShiftRelease(version, outputPath string) error {
 	}
 	defer func() { _ = resp.Body.Close() }()
 
-	assetID := getOpenShiftServerAssetID(release)
+	assetID, filename := getOpenShiftServerAssetID(release)
 	if assetID == 0 {
 		return errors.New("Could not get OpenShift release URL")
 	}
+	var asset io.Reader
 	asset, url, err := client.Repositories.DownloadReleaseAsset("openshift", "origin", assetID)
 	if err != nil {
 		return errors.Wrap(err, "Could not download OpenShift release asset")
@@ -113,11 +117,22 @@ func DownloadOpenShiftRelease(version, outputPath string) error {
 		}
 	}
 
+	hasher := sha256.New()
+
+	asset = io.TeeReader(asset, hasher)
+
 	gzf, err := gzip.NewReader(asset)
 	if err != nil {
 		return errors.Wrap(err, "Could not ungzip OpenShift release asset")
 	}
 	defer func() { _ = gzf.Close() }()
+	destDir := filepath.Dir(outputPath)
+	err = os.MkdirAll(destDir, 0755)
+	if err != nil && !os.IsExist(err) {
+		return errors.Wrap(err, "Could not create target directory")
+	}
+	destFileName := filepath.Base(outputPath)
+	tmp, err := ioutil.TempFile(destDir, ".tmp-"+destFileName)
 	tr := tar.NewReader(gzf)
 	for {
 		hdr, err := tr.Next()
@@ -126,33 +141,94 @@ func DownloadOpenShiftRelease(version, outputPath string) error {
 			break
 		}
 		if err != nil {
+			os.Remove(tmp.Name())
 			return errors.Wrap(err, "Could not extract OpenShift release asset")
 		}
 		if hdr.Typeflag != tar.TypeReg || filepath.Base(hdr.Name) != "kube-apiserver" {
 			continue
 		}
-		destDir := filepath.Dir(outputPath)
-		err = os.MkdirAll(destDir, 0755)
-		if err != nil && !os.IsExist(err) {
-			return errors.Wrap(err, "Could not create target directory")
-		}
-		destFileName := filepath.Base(outputPath)
-		tmp, err := ioutil.TempFile(destDir, ".tmp-"+destFileName)
 		_, err = io.Copy(tmp, tr)
 		if err != nil {
+			os.Remove(tmp.Name())
 			return errors.Wrap(err, "Could not extract OpenShift release asset")
 		}
-		err = os.Rename(tmp.Name(), outputPath)
-		if err != nil {
-			return errors.Wrap(err, "Could not write OpenShift binary")
-		}
+	}
+	hash := hex.EncodeToString(hasher.Sum(nil))
+	downloadedHash, err := downloadHash(release, filename)
+	if err != nil {
+		os.Remove(tmp.Name())
+		return errors.Wrap(err, "Failed to download hash")
+	}
+	if len(downloadedHash) == 0 {
+		os.Remove(tmp.Name())
+		return errors.New("File has no hash to validate - not downloading")
+	}
+
+	if hash != downloadedHash {
+		os.Remove(tmp.Name())
+		return errors.Errorf("Failed to validate hash - expected: %s, actual: %s", hash, downloadedHash)
+	}
+
+	err = os.Rename(tmp.Name(), outputPath)
+	if err != nil {
+		os.Remove(tmp.Name())
+		return errors.Wrap(err, "Could not write OpenShift binary")
 	}
 	return nil
 }
 
-func getOpenShiftServerAssetID(release *github.RepositoryRelease) int {
+func getOpenShiftServerAssetID(release *github.RepositoryRelease) (int, string) {
 	for _, asset := range release.Assets {
 		if strings.HasPrefix(*asset.Name, "openshift-origin-server") && strings.HasSuffix(*asset.Name, "linux-64bit.tar.gz") {
+			return *asset.ID, *asset.Name
+		}
+	}
+	return 0, ""
+}
+
+func downloadHash(release *github.RepositoryRelease, filename string) (string, error) {
+	assetID := getOpenShiftChecksumAssetID(release)
+	if assetID == 0 {
+		return "", errors.New("Could not get OpenShift release checksum URL")
+	}
+	var asset io.Reader
+	asset, url, err := client.Repositories.DownloadReleaseAsset("openshift", "origin", assetID)
+	if err != nil {
+		return "", errors.Wrap(err, "Could not download OpenShift release checksum asset")
+	}
+	if len(url) > 0 {
+		fmt.Printf("Downloading OpenShift %s checksums\n", *release.TagName)
+		httpResp, err := http.Get(url)
+		if err != nil {
+			return "", errors.Wrap(err, "Could not download OpenShift release checksum asset")
+		}
+		defer func() { _ = httpResp.Body.Close() }()
+
+		asset = httpResp.Body
+		if httpResp.ContentLength > 0 {
+			bar := pb.New64(httpResp.ContentLength).SetUnits(pb.U_BYTES)
+			bar.Start()
+			asset = bar.NewProxyReader(asset)
+			defer func() {
+				<-time.After(bar.RefreshRate)
+				fmt.Println()
+			}()
+		}
+	}
+
+	scanner := bufio.NewScanner(asset)
+	for scanner.Scan() {
+		spl := strings.Fields(scanner.Text())
+		if len(spl) == 2 && spl[1] == filename {
+			return spl[0], nil
+		}
+	}
+	return "", nil
+}
+
+func getOpenShiftChecksumAssetID(release *github.RepositoryRelease) int {
+	for _, asset := range release.Assets {
+		if *asset.Name == "CHECKSUM" {
 			return *asset.ID
 		}
 	}

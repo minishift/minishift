f489efc83f89356b4f7a475f9fcfcaebc58fdd6d
Budh Ram Gurung <budhram.gurung01@gmail.com>
2017-02-21 07:52:16 +0530

    Issue #443 gofmtcheck fails in master
    * Also refactored the code

--- pkg/minishift/util/proxy_util.go
+++ pkg/minishift/util/proxy_util.go
@@ -23,47 +23,47 @@ import (
 	"github.com/golang/glog"
 )
 
-//Parses the proxy URI and will return  ["proxy_hostname", "proxy_port", "user", "password"]
-//For unauthenticated proxy server it will return ["proxy_hostname:proxy_port", "user"]
-//Or ["proxy_hostname:proxy_port"] depending upon the URI
-func ParseProxyUri(proxyUri string) ([]string, error) {
-	var (
-		proxyUserInfoSlice []string
-		proxyUriSlice      []string
-		matched            bool
-		err                error
-	)
+var (
+	proxyUserInfoSlice []string
+	proxyUriSlice      []string
+	matched            bool
+	err                error
+)
 
+// Parses the proxy URI and will return  ["proxy_hostname", "proxy_port", "user", "password"]
+// For unauthenticated proxy server it will return ["proxy_hostname:proxy_port", "user"]
+// Or ["proxy_hostname:proxy_port"] depending upon the URI
+func ParseProxyUri(proxyUri string) ([]string, error) {
 	if matched, err = regexp.MatchString("http://", proxyUri); matched {
 		proxyUri = strings.TrimPrefix(proxyUri, "http://")
 	} else if matched, err = regexp.MatchString("https://", proxyUri); matched {
 		proxyUri = strings.TrimPrefix(proxyUri, "https://")
 	}
+
 	if err != nil {
 		glog.Errorf("Error starting the VM: %s. Retrying.\n", err)
 		return nil, err
 	}
 
-	//Get rid of the trailing "/" if any
+	// Get rid of the trailing "/" if any
 	proxyUri = strings.TrimSuffix(proxyUri, "/")
 
-	//Value of proxyUri at this time would be similar to fedora:fedora123@xyz.com:8000
-	//splitting the string at the last occurence of "@". As we know proxy URL will not contain "@"
+	// Value of proxyUri at this time would be similar to fedora:fedora123@xyz.com:8000
+	// splitting the string at the last occurence of "@". As we know proxy URL will not contain "@"
 	index := strings.LastIndex(proxyUri, "@")
 
-	//For an unauthenticated proxy username and password will not be present
+	// For an unauthenticated proxy username and password will not be present
 	if index != -1 {
 		proxyUserInfo := strings.TrimSuffix(proxyUri[0:index+1], "@")
 		proxyUserInfoSlice = strings.Split(proxyUserInfo, ":")
 	}
 
-	proxyUrl := proxyUri[index+1 : len(proxyUri)]
+	proxyUrl := proxyUri[index+1:]
 	proxyUrlSlice := strings.Split(proxyUrl, ":")
 
 	if proxyUserInfoSlice != nil {
 		proxyUriSlice = append(proxyUrlSlice, proxyUserInfoSlice...)
 		return proxyUriSlice, nil
-	} else {
-		return proxyUrlSlice, nil
 	}
+	return proxyUrlSlice, nil
 }

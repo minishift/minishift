7350b482a9dfd57f6794ca754b74683208c61cca
Hardy Ferentschik <hardy@hibernate.org>
2017-04-08 23:01:05 +0200

    Issue #647 Make sure asciidoctor include works for Middleman as well as AsciiBinder site build

--- docs/config.rb
+++ docs/config.rb
@@ -17,55 +17,27 @@ page '/*.txt', layout: false
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
 # General configuration
-activate :asciidoc
+activate :asciidoc, attributes: {"icons" => "font"}
 
-set :asciidoc_attributes, %w(icons=font)
+class AsciidoctorTweaks < Middleman::Extension
+  def initialize app, options_hash = {}, &block
+    super
+    app.before do
+      app.config[:asciidoc][:base_dir] = app.source_dir
+      true
+    end
+  end
+end
+
+::Middleman::Extensions.register :asciidoctor_tweaks, AsciidoctorTweaks
+
+activate :asciidoctor_tweaks
 
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
 end
 
-class CustomMarkdown < Middleman::Extension
-  $markdown_options = {
-    autolink:           true,
-    fenced_code_blocks: true,
-    no_intra_emphasis:  true,
-    strikethrough:      true,
-    tables:             true,
-    hard_wrap:          false,
-    with_toc_data:      true
-  }
-
-  # Markdown files
-  def initialize(app, options_hash={}, &block)
-    super
-    app.set :markdown_engine, :redcarpet
-    app.set :markdown, $markdown_options
-  end
-
-  # TOC helper
-  helpers do
-    # Based on https://github.com/vmg/redcarpet/pull/186#issuecomment-22783188
-    def toc(page)
-      html_toc = Redcarpet::Markdown.new(Redcarpet::Render::HTML_TOC)
-      file = ::File.read(page.source_file)
-
-      # remove YAML frontmatter
-      file = file.gsub(/^(---\s*\n.*?\n?)^(---\s*$\n?)/m,'')
-
-      # quick fix for HAML: remove :markdown filter and indentation
-      file = file.gsub(/:markdown\n/,'')
-      file = file.gsub(/\t/,'')
-
-      html_toc.render file
-    end
-  end
-end
-
-::Middleman::Extensions.register(:custom_markdown, CustomMarkdown)
-
-activate :custom_markdown
 activate :syntax, :line_numbers => true
 
 # Build-specific configuration

a835acf5d54cb88d0724c2ccc2bfaa3f448abc88
Budh Ram Gurung <budhram.gurung01@gmail.com>
2017-04-09 18:41:09 +0530

    Issue #535 CentOS CI changes for docs artifacts uploads

--- centos_ci.sh
+++ centos_ci.sh
@@ -22,79 +22,176 @@ set -e
 
 # Source environment variables of the jenkins slave
 # that might interest this worker.
-if [ -e "jenkins-env" ]; then
-  cat jenkins-env \
-    | grep -E "(JENKINS_URL|GIT_BRANCH|GIT_COMMIT|BUILD_NUMBER|ghprbSourceBranch|ghprbActualCommit|BUILD_URL|ghprbPullId|GH_TOKEN|CICO_API_KEY)=" \
-    | sed 's/^/export /g' \
-    > ~/.jenkins-env
-  source ~/.jenkins-env
-fi
+function load_jenkins_vars() {
+  if [ -e "jenkins-env" ]; then
+    cat jenkins-env \
+      | grep -E "(JENKINS_URL|GIT_BRANCH|GIT_COMMIT|BUILD_NUMBER|ghprbSourceBranch|ghprbActualCommit|BUILD_URL|ghprbPullId|GH_TOKEN|CICO_API_KEY|JOB_NAME)=" \
+      | sed 's/^/export /g' \
+      > ~/.jenkins-env
+    source ~/.jenkins-env
+  fi
 
-# We need to disable selinux for now, XXX
-/usr/sbin/setenforce 0
+  echo 'CICO: Jenkins ENVs loaded'
+}
 
-# Get all the deps in
-yum -y install \
-  gcc \
-  make \
-  git \
-  curl \
-  kvm \
-  qemu-kvm \
-  libvirt
+function install_core_deps() {
+  # We need to disable selinux for now, XXX
+  /usr/sbin/setenforce 0
 
-# Start Libvirt
-sudo systemctl start libvirtd
+  # Get all the deps in
+  yum -y install gcc \
+                 make \
+                 git \
+                 curl
 
-# Setup 'kvm' driver
-curl -L https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.7.0/docker-machine-driver-kvm > /usr/local/bin/docker-machine-driver-kvm && \
-chmod +x /usr/local/bin/docker-machine-driver-kvm
+  echo 'CICO: Core dependencies installed'
+}
 
-# Install Go 1.7
-curl -LO https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz
-tar -xf go1.7.linux-amd64.tar.gz
-mv go /usr/local
+function install_kvm_virt() {
+  sudo yum -y install kvm \
+                      qemu-kvm \
+                      libvirt
+  # Start Libvirt
+  sudo systemctl start libvirtd
+  echo 'CICO: KVM hypervisor installed and started'
 
-# Setup GOROOT
-export GOROOT=/usr/local/go
-# Setup GOPATH
-mkdir $HOME/gopath $HOME/gopath/src $HOME/gopath/bin $HOME/gopath/pkg
-export GOPATH=$HOME/gopath
-export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
+  # Add minishift_ci to libvirt group
+  gpasswd -a minishift_ci libvirt && systemctl restart libvirtd
+}
 
-# Setup minishift repo
-mkdir -p $GOPATH/src/github.com/minishift
-ln -s `pwd` $GOPATH/src/github.com/minishift/minishift
-cd $GOPATH/src/github.com/minishift/minishift
+function install_docker() {
+  yum install -y docker
+  systemctl start docker
 
-# Setup Glide ("curl https://glide.sh/get | sh" not working due to https://github.com/Masterminds/glide/issues/708)
-GLIDE_OS_ARCH=`go env GOHOSTOS`-`go env GOHOSTARCH`
-GLIDE_TAG=v0.12.3
-GLIDE_LATEST_RELEASE_URL="https://github.com/Masterminds/glide/releases/download/${GLIDE_TAG}/glide-${GLIDE_TAG}-${GLIDE_OS_ARCH}.tar.gz"
-curl -LO ${GLIDE_LATEST_RELEASE_URL}
-mkdir /tmp/glide
-tar --directory=/tmp/glide -xvf glide-${GLIDE_TAG}-${GLIDE_OS_ARCH}.tar.gz
-export PATH=$PATH:/tmp/glide/${GLIDE_OS_ARCH}
+  docker version
+  echo 'CICO: Docker installed and started'
 
-make clean test cross fmtcheck prerelease
-# Run integration test with 'kvm' driver
-MINISHIFT_VM_DRIVER=kvm make integration
+  # Add minishift_ci to docker group
+  groupadd docker && gpasswd -a minishift_ci docker && systemctl restart docker
+}
 
-# On reaching successfully at this point, upload artifacts
-PASS=$(echo $CICO_API_KEY | cut -d'-' -f1-2)
+# Create a docs user which has NOPASSWD sudoer role
+function prepare_ci_user() {
+  groupadd -r minishift_ci && useradd -g minishift_ci minishift_ci
+  chmod +w /etc/sudoers && echo "minishift_ci ALL=(ALL)    NOPASSWD: ALL" >> /etc/sudoers && chmod -w /etc/sudoers
 
-set +x
-# For PR build, GIT_BRANCH is set to branch name other than origin/master
-if [[ "$GIT_BRANCH" = "origin/master" ]]; then
+  # Copy centos_ci.sh to newly created user home dir
+  cp centos_ci.sh /home/minishift_ci/
+  mkdir /home/minishift_ci/payload
+  # Copy minishift repo content into minishift_ci user payload directory for later use
+  cp -R . /home/minishift_ci/payload
+  chown -R minishift_ci:minishift_ci /home/minishift_ci/payload
+
+  # Copy the jenkins-env into minishift_ci home dir
+  cp ~/.jenkins-env /home/minishift_ci/jenkins-env
+}
+
+####### Below functions are executed by minishift_ci user
+function setup_kvm_docker_machine_driver() {
+  curl -L https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.7.0/docker-machine-driver-kvm > docker-machine-driver-kvm && \
+  chmod +x docker-machine-driver-kvm && sudo mv docker-machine-driver-kvm /usr/local/bin/docker-machine-driver-kvm
+  echo 'CICO: Setup KVM docker-machine driver setup successfully'
+}
+
+function install_and_setup_golang() {
+  # Install Go 1.7
+  curl -LO https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz
+  tar -xf go1.7.linux-amd64.tar.gz
+  sudo mv go /usr/local
+
+  # Setup GOROOT
+  export GOROOT=/usr/local/go
+  # Setup GOPATH
+  mkdir $HOME/gopath $HOME/gopath/src $HOME/gopath/bin $HOME/gopath/pkg
+  export GOPATH=$HOME/gopath
+  export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
+}
+
+function setup_repo() {
+  # Setup minishift repo
+  mkdir -p $GOPATH/src/github.com/minishift
+  cp -r /home/minishift_ci/payload $GOPATH/src/github.com/minishift/minishift
+}
+
+function setup_glide() {
+  # Setup Glide ("curl https://glide.sh/get | sh" not working due to https://github.com/Masterminds/glide/issues/708)
+  GLIDE_OS_ARCH=`go env GOHOSTOS`-`go env GOHOSTARCH`
+  GLIDE_TAG=v0.12.3
+  GLIDE_LATEST_RELEASE_URL="https://github.com/Masterminds/glide/releases/download/${GLIDE_TAG}/glide-${GLIDE_TAG}-${GLIDE_OS_ARCH}.tar.gz"
+  curl -LO ${GLIDE_LATEST_RELEASE_URL}
+  mkdir /tmp/glide
+  tar --directory=/tmp/glide -xvf glide-${GLIDE_TAG}-${GLIDE_OS_ARCH}.tar.gz
+  export PATH=$PATH:/tmp/glide/${GLIDE_OS_ARCH}
+}
+
+function prepare() {
+  echo "UID in prepare: $UID"
+  install_and_setup_golang;
+  setup_repo;
+  setup_glide;
+
+  echo "CICO: Preparation complete"
+}
+
+function run_tests() {
+  cd $GOPATH/src/github.com/minishift/minishift
+  make clean test cross fmtcheck prerelease
+  # Run integration test with 'kvm' driver
+  MINISHIFT_VM_DRIVER=kvm make integration
+
+  echo "CICO: Tests ran successfully"
+}
+
+function artifacts_upload_on_pr_and_master_trigger() {
+  set +x
+  # For PR build, GIT_BRANCH is set to branch name other than origin/master
+  if [[ "$GIT_BRANCH" = "origin/master" ]]; then
+    # http://stackoverflow.com/a/22908437/1120530; Using --relative as --rsync-path not working
+    mkdir -p minishift/master/$BUILD_NUMBER/
+    cp -r out/*-amd64 minishift/master/$BUILD_NUMBER/
+    RSYNC_PASSWORD=$1 rsync -a --delete --relative minishift/master/$BUILD_NUMBER/ minishift@artifacts.ci.centos.org::minishift/
+    echo "Find Artifacts here http://artifacts.ci.centos.org/minishift/minishift/master/$BUILD_NUMBER ."
+  else
+    # http://stackoverflow.com/a/22908437/1120530; Using --relative as --rsync-path not working
+    mkdir -p minishift/pr/$ghprbPullId/
+    cp -r out/*-amd64 minishift/pr/$ghprbPullId/
+    RSYNC_PASSWORD=$1 rsync -a --delete --relative minishift/pr/$ghprbPullId/ minishift@artifacts.ci.centos.org::minishift/
+    echo "Find Artifacts here http://artifacts.ci.centos.org/minishift/minishift/pr/$ghprbPullId ."
+  fi
+}
+
+function docs_tar_upload() {
+  set -x
+
+  version=$(cat docs/build/variables.adoc | cut -d' ' -f2 | head -n1)
+  mkdir -p minishift/docs/$version
+  cp docs/build/minishift-adoc.tar minishift/docs/$version/
+  ln -s $(pwd)/minishift/docs/$version minishift/docs/latest
   # http://stackoverflow.com/a/22908437/1120530; Using --relative as --rsync-path not working
-  mkdir -p minishift/master/$BUILD_NUMBER/
-  cp -r out/* minishift/master/$BUILD_NUMBER/
-  RSYNC_PASSWORD=$PASS rsync -a --relative minishift/master/$BUILD_NUMBER/ minishift@artifacts.ci.centos.org::minishift/
-  echo "Find Artifacts here http://artifacts.ci.centos.org/minishift/minishift/master/$BUILD_NUMBER ."
+  RSYNC_PASSWORD=$1 rsync -aL --relative minishift/docs minishift@artifacts.ci.centos.org::minishift/
+  echo "Find docs tar here http://artifacts.ci.centos.org/minishift/minishift/docs/latest."
+}
+
+if [[ "$UID" = 0 ]]; then
+  load_jenkins_vars;
+  prepare_ci_user;
+  install_core_deps;
+  install_kvm_virt;
+  install_docker;
+  runuser -l minishift_ci -c "/bin/bash centos_ci.sh"
 else
-  # http://stackoverflow.com/a/22908437/1120530; Using --relative as --rsync-path not working
-  mkdir -p minishift/pr/$ghprbPullId/
-  cp -r out/* minishift/pr/$ghprbPullId/
-  RSYNC_PASSWORD=$PASS rsync -a --relative minishift/pr/$ghprbPullId/ minishift@artifacts.ci.centos.org::minishift/
-  echo "Find Artifacts here http://artifacts.ci.centos.org/minishift/minishift/pr/$ghprbPullId ."
+  source ~/jenkins-env # Source environment variables for minishift_ci user
+  PASS=$(echo $CICO_API_KEY | cut -d'-' -f1-2)
+
+  if [[ "$JOB_NAME" = "minishift-docs" ]]; then
+    prepare;
+    cd gopath/src/github.com/minishift/minishift
+    make gen_adoc_tar
+    docs_tar_upload $PASS
+  else
+    setup_kvm_docker_machine_driver;
+    prepare;
+    run_tests;
+    artifacts_upload_on_pr_and_master_trigger $PASS;
+  fi
 fi

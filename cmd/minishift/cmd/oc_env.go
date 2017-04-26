9d12cebf803191d3950b6141b5acc1bb5626321f
Budh Ram Gurung <budhram.gurung01@gmail.com>
2017-04-26 16:13:06 +0530

    [WIP] Issue #142 Added oc-env command
    Need to add tests yet

---
+++ cmd/minishift/cmd/oc_env.go
@@ -0,0 +1,98 @@
+/*
+Copyright (C) 2017 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"github.com/minishift/minishift/pkg/minikube/constants"
+
+	"github.com/docker/machine/libmachine"
+	"github.com/minishift/minishift/cmd/minishift/cmd/util"
+	instanceState "github.com/minishift/minishift/pkg/minishift/config"
+	"github.com/minishift/minishift/pkg/util/os/atexit"
+	"github.com/minishift/minishift/pkg/util/shell"
+	"github.com/spf13/cobra"
+	"os"
+	"path"
+	"text/template"
+)
+
+const (
+	ocEnvTmpl = `{{ .Prefix }}PATH{{ .Delimiter }}{{ .OcPath }}{{ .Suffix }}{{ .UsageHint }}`
+)
+
+type OcShellConfig struct {
+	shell.ShellConfig
+	OcPath    string
+	UsageHint string
+}
+
+func getOcShellConfig(api libmachine.API, forceShell string) (*OcShellConfig, error) {
+	userShell, err := shell.GetShell(forceShell)
+	if err != nil {
+		return nil, err
+	}
+
+	cmdLine := "minishift oc-env"
+	shellCfg := &OcShellConfig{
+		OcPath:    path.Dir(instanceState.InstanceConfig.OcPath),
+		UsageHint: shell.GenerateUsageHint(userShell, cmdLine),
+	}
+
+	prefix, suffix, delimiter := shell.GetPrefixSuffixDelimiterForSet(userShell, true)
+	shellCfg.Prefix = prefix
+	shellCfg.Suffix = suffix
+	shellCfg.Delimiter = delimiter
+
+	return shellCfg, nil
+}
+
+func executeOcTemplateStdout(shellCfg *OcShellConfig) error {
+	tmpl := template.Must(template.New("envConfig").Parse(ocEnvTmpl))
+	return tmpl.Execute(os.Stdout, shellCfg)
+}
+
+var ocEnvCmd = &cobra.Command{
+	Use:   "oc-env",
+	Short: "Sets path of 'oc' binary.",
+	Long:  `Sets path of OpenShift client binary, 'oc'.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		api := libmachine.NewClient(constants.Minipath, constants.MakeMiniPath("certs"))
+		defer api.Close()
+
+		util.ExitIfUndefined(api, constants.MachineName)
+		_, err := api.Load(constants.MachineName)
+		if err != nil {
+			atexit.ExitWithMessage(1, err.Error())
+		}
+
+		var shellCfg *OcShellConfig
+
+		shellCfg, err = getOcShellConfig(api, forceShell)
+		if err != nil {
+			atexit.ExitWithMessage(1, fmt.Sprintf("Error running oc-env command: %s", err.Error()))
+		}
+
+		executeOcTemplateStdout(shellCfg)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(ocEnvCmd)
+	ocEnvCmd.Flags().StringVar(&forceShell, "shell", "", "Force setting the environment for a specified shell: [fish, cmd, powershell, tcsh, bash, zsh]. Default is auto-detect.")
+}

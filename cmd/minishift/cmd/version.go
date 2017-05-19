03ce8b69ec83be964c634e882aa2996c2280ed37
Casey Fulton <casey@caseyfulton.com>
2017-05-19 15:07:50 +0930

    Changes version output format to "minishift vX.X.X".

--- cmd/minishift/cmd/version.go
+++ cmd/minishift/cmd/version.go
@@ -32,7 +32,7 @@ var versionCmd = &cobra.Command{
 		RootCmd.PersistentPreRun(cmd, args)
 	},
 	Run: func(command *cobra.Command, args []string) {
-		fmt.Println("Minishift version:", version.GetVersion())
+		fmt.Println("minishift", version.GetVersion())
 	},
 }
 

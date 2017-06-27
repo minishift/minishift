76460970c658f34e99a1bc0ed320ef1b31daeac0
Hardy Ferentschik <hardy@hibernate.org>
2017-06-27 12:33:11 +0200

    Issue #1042 Making sure images can be used in dev mode as well as docs.openshift.orgw

--- docs/config.rb
+++ docs/config.rb
@@ -9,6 +9,8 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+set :relative_links, true
+
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
 
@@ -19,32 +21,9 @@ page '/*.txt', layout: false
 # General configuration
 activate :asciidoc, attributes: {"icons" => "font"}
 
-class AsciidoctorTweaks < Middleman::Extension
-  def initialize app, options_hash = {}, &block
-    super
-    app.before do
-      app.config[:asciidoc][:base_dir] = app.source_dir
-      true
-    end
-  end
-end
-
-::Middleman::Extensions.register :asciidoctor_tweaks, AsciidoctorTweaks
-
-activate :asciidoctor_tweaks
-
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
 end
 
 activate :syntax, :line_numbers => true
-
-# Build-specific configuration
-configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-end

f8ab1f9fa7813897d0b74f06afaca5f503e8ec03
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2017-07-10 13:00:44 +0530

    Issue #1106 Incorporate NodePort into minishift openshift service command

--- cmd/minishift/cmd/openshift/service.go
+++ cmd/minishift/cmd/openshift/service.go
@@ -19,20 +19,24 @@ package openshift
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/docker/machine/libmachine"
 	"github.com/minishift/minishift/cmd/minishift/cmd/util"
 	"github.com/minishift/minishift/pkg/minikube/constants"
 	"github.com/minishift/minishift/pkg/minishift/openshift"
 	"github.com/minishift/minishift/pkg/util/os/atexit"
+	"github.com/olekukonko/tablewriter"
 	"github.com/pkg/browser"
 	"github.com/spf13/cobra"
 )
 
 var (
 	namespace string
-	urlMode   bool
+	inbrowser bool
 	https     bool
+	url       bool
+	service   string
 )
 
 // serviceCmd represents the service command
@@ -50,25 +54,120 @@ var serviceCmd = &cobra.Command{
 			atexit.ExitWithMessage(1, "You must specify the name of the service.")
 		}
 
-		service := args[0]
-
-		url, err := openshift.GetServiceSpec(service, namespace, https)
+		host, err := api.Load(constants.MachineName)
 		if err != nil {
 			atexit.ExitWithMessage(1, err.Error())
 		}
 
-		if urlMode {
-			fmt.Fprintln(os.Stdout, url)
+		util.ExitIfNotRunning(host.Driver, constants.MachineName)
+
+		ip, err := host.Driver.GetIP()
+		if err != nil {
+			atexit.ExitWithMessage(1, fmt.Sprintf("Error getting IP: %s", err.Error()))
+		}
+
+		service = args[0]
+
+		serviceSpecs, err := openshift.GetServiceSpecs(namespace)
+		if err != nil {
+			atexit.ExitWithMessage(1, err.Error())
+		}
+
+		if url {
+			stdOutURL(serviceSpecs, ip)
+		} else if inbrowser {
+			openInBrowser(serviceSpecs, ip)
 		} else {
-			fmt.Fprintln(os.Stdout, "Opening the service "+namespace+"/"+service+" in the default browser...")
-			browser.OpenURL(url)
+			printToStdOut(serviceSpecs, ip)
 		}
 	},
 }
 
 func init() {
-	serviceCmd.Flags().StringVarP(&namespace, "namespace", "n", "default", "The namespace of the service.")
-	serviceCmd.Flags().BoolVar(&urlMode, "url", false, "Access the service in the command-line console instead of in the default browser.")
+	serviceCmd.Flags().StringVarP(&namespace, "namespace", "n", "", "The namespace of the service.")
+	serviceCmd.Flags().BoolVar(&inbrowser, "in-browser", false, "Access the service in the default browser.")
+	serviceCmd.Flags().BoolVar(&url, "url", false, "Print the service URL to standard output.")
 	serviceCmd.Flags().BoolVar(&https, "https", false, "Access the service with HTTPS instead of HTTP.")
 	OpenShiftCmd.AddCommand(serviceCmd)
+}
+
+func openInBrowser(serviceSpecs []openshift.ServiceSpec, ip string) {
+	serviceURL := getServiceURL(serviceSpecs, ip)
+	fmt.Fprintln(os.Stdout, "Opening the route/NodePort "+serviceURL+" in the default browser...")
+	browser.OpenURL(serviceURL)
+}
+
+func stdOutURL(serviceSpecs []openshift.ServiceSpec, ip string) {
+	serviceURL := getServiceURL(serviceSpecs, ip)
+	fmt.Fprintln(os.Stdout, serviceURL)
+}
+
+func getServiceURL(serviceSpecs []openshift.ServiceSpec, ip string) string {
+	serviceURL := ""
+	namespaceList := isServiceInMultipleNamespace(serviceSpecs, service)
+	if len(namespaceList) == 0 {
+		atexit.ExitWithMessage(1, fmt.Sprintf("Service %s does not exist", service))
+	}
+	if len(namespaceList) > 1 {
+		namespaces := strings.TrimSpace(strings.Join(namespaceList, ", "))
+		atexit.ExitWithMessage(1, fmt.Sprintf("Service %s exists in multiple namespaces (%s), you need to chose a specific namespace using -n <namespace>.", service, namespaces))
+	}
+
+	for _, serviceSpec := range serviceSpecs {
+		if serviceSpec.Name == service {
+			if serviceSpec.URL != nil {
+				serviceURL = serviceSpec.URL[0]
+				return serviceURL
+
+			} else if serviceSpec.NodePort != "" {
+				nodePortURL := fmt.Sprintf("%s:%s", ip, serviceSpec.NodePort)
+				urlScheme := "http://"
+				if https {
+					urlScheme = "https://"
+				}
+				serviceURL = urlScheme + nodePortURL
+				return serviceURL
+			} else {
+				atexit.ExitWithMessage(1, fmt.Sprintf("Service '%s' in namespace '%s' does not have route associated which can be opened in the browser.", serviceSpec.Name, serviceSpec.Namespace))
+			}
+		}
+	}
+	return serviceURL
+}
+
+func isServiceInMultipleNamespace(serviceSpecs []openshift.ServiceSpec, service string) []string {
+	namespceList := []string{}
+	for _, serviceSpec := range serviceSpecs {
+		if serviceSpec.Name == service {
+			namespceList = append(namespceList, serviceSpec.Namespace)
+		}
+	}
+	return namespceList
+}
+
+func printToStdOut(serviceSpecs []openshift.ServiceSpec, ip string) {
+	var data [][]string
+	var urls, weights string
+
+	for _, serviceSpec := range serviceSpecs {
+		if serviceSpec.Name == service {
+			nodePortURL := serviceSpec.NodePort
+			if nodePortURL != "" {
+				nodePortURL = fmt.Sprintf("%s:%s", ip, nodePortURL)
+			}
+			if serviceSpec.URL != nil {
+				urls = strings.Join(serviceSpec.URL, "\n")
+			}
+			if serviceSpec.Weight != nil {
+				weights = strings.Join(serviceSpec.Weight, "\n")
+			}
+			data = append(data, []string{serviceSpec.Namespace, serviceSpec.Name, nodePortURL, urls, weights})
+		}
+	}
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Namsepace", "Name", "NodePort", "Route-URL", "Weight"})
+	table.SetBorders(tablewriter.Border{Left: true, Top: true, Right: true, Bottom: true})
+	table.SetCenterSeparator("|")
+	table.AppendBulk(data)
+	table.Render()
 }

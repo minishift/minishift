minishift/minishift

commit c2aeff6604c57a7caaaf36ab534cce2053a2fafd

Issue #1518 Profile set should create the profile and set it as active profile

cmd/minishift/cmd/profile/profile_set.go
@@ -34,28 +34,33 @@ var profileSetCmd = &cobra.Command{
 	Use:   "set PROFILE_NAME",
 	Short: "Sets the active profile for Minishift.",
 	Long:  "Sets the active profile for Minishift. After you set the profile, all commands will use the specified profile by default.",
-	Run: func(cmd *cobra.Command, args []string) {
-		validateArgs(args)
-		profileName := args[0]
+	Run:   runProfile,
+}
 
-		if !cmdUtil.IsValidProfile(profileName) {
-			atexit.ExitWithMessage(1, fmt.Sprintf("Error: '%s' is not a valid profile", profileName))
-		}
+func runProfile(cmd *cobra.Command, args []string) {
+	validateArgs(args)
+	profileName := args[0]
 
+	if !cmdUtil.IsValidProfile(profileName) {
 		err := profileActions.SetActiveProfile(profileName)
 		if err != nil {
 			atexit.ExitWithMessage(1, err.Error())
 		}
-		fmt.Println(fmt.Sprintf("Profile '%s' set as active profile", profileName))
-
-		err = cmdUtil.SetOcContext(profileName)
+	} else {
+		err := cmdUtil.SetOcContext(profileName)
 		if err != nil {
 			if glog.V(2) {
 				fmt.Println(fmt.Sprintf("%s", err.Error()))
 			}
-			fmt.Println(fmt.Sprintf("oc cli context could not changed for '%s'. %s", profileName, unableToSetOcContextErrorMessage))
+			atexit.ExitWithMessage(1, fmt.Sprintf("oc cli context could not changed for '%s'. %s", profileName, unableToSetOcContextErrorMessage))
 		}
-	},
+
+		err = profileActions.SetActiveProfile(profileName)
+		if err != nil {
+			atexit.ExitWithMessage(1, err.Error())
+		}
+	}
+	fmt.Printf("Profile '%s' set as active profile\n", profileName)
 }
 
 func init() {

cmd/minishift/cmd/util/util.go
@@ -87,17 +87,13 @@ func UnpackAddons(addonsDir string) error {
 	return nil
 }
 
+// IsValidProfile return true if a given profile exist
 func IsValidProfile(profileName string) bool {
-	var doesProfileExist bool
 	profileList := profileActions.GetProfileList()
 	for i := range profileList {
 		if profileList[i] == profileName {
-			doesProfileExist = true
-			break
+			return true
 		}
 	}
-	if !doesProfileExist {
-		return false
-	}
-	return true
+	return false
 }

commit 90a830e507c2d5a3c9ebc2c6414da97f0e977e25

Issue #1574, #1577 Fixing profile set to work always
This issue was introduced in commit c2aeff6

Signed-off-by: Lalatendu Mohanty <lmohanty@redhat.com>

cmd/minishift/cmd/profile/profile_set.go
@@ -52,7 +52,7 @@ func runProfile(cmd *cobra.Command, args []string) {
 			if glog.V(2) {
 				fmt.Println(fmt.Sprintf("%s", err.Error()))
 			}
-			atexit.ExitWithMessage(1, fmt.Sprintf("oc cli context could not changed for '%s'. %s", profileName, unableToSetOcContextErrorMessage))
+			fmt.Println(fmt.Sprintf("oc cli context could not changed for '%s'. %s", profileName, unableToSetOcContextErrorMessage))
 		}
 
 		err = profileActions.SetActiveProfile(profileName)

test/integration/features/profile.feature
@@ -71,3 +71,21 @@ Scenario: Deleting Minishift
     Then Minishift should have state "Does Not Exist"
     When executing "minishift ip"
     Then exitcode should equal "1"
+
+Scenario: User should be able to switch between non existing profiles
+    When executing "minishift profile list" succeeds
+    Then stdout should contain
+    """
+    - minishift	Does Not Exist	(active)
+    """
+    When executing "minishift profile set abc" succeeds
+      Then stdout should contain
+      """
+      Profile 'abc' set as active profile
+      """
+    When executing "minishift profile set minishift" succeeds
+      Then stdout should contain
+      """
+      oc cli context could not changed for 'minishift'. Make sure the profile is in running state or restart if the problem persists.
+      Profile 'minishift' set as active profile
+      """
c3bec3d06e5166139dceca67a6973b58a5d711e1
ulyssessouza <ulyssessouza@gmail.com>
2017-11-17 09:05:47 +0100

    Issue #1662 Add profile line into start log

--- cmd/minishift/cmd/start.go
+++ cmd/minishift/cmd/start.go
@@ -165,6 +165,7 @@ func runStart(cmd *cobra.Command, args []string) {
 
 	proxyConfig := handleProxies()
 
+	fmt.Println(fmt.Sprintf("-- Starting profile '%s'", constants.ProfileName))
 	fmt.Printf("-- Starting local OpenShift cluster")
 	hostVm := startHost(libMachineClient)
 	registrationUtil.RegisterHost(libMachineClient)

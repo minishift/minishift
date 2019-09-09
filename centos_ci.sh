47da63e001c12f1faa3ffd0abf494ce58bcba5aa
Anjan Nath <kaludios@gmail.com>
2019-09-09 12:13:02 +0530

    Issue #3339 Fetch gpg keys for rvm from their website

--- centos_ci.sh
+++ centos_ci.sh
@@ -163,7 +163,8 @@ function install_docs_prerequisite_packages() {
                      java-1.7.0-openjdk-devel
 
   # Install RVM
-  gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+  curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+  curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
   curl -L get.rvm.io | bash -s stable
   source ~/.profile
   # Install Ruby
